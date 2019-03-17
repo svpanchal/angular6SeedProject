2a00514b7168d30e23e1eecce2f39f3e61854d93
svpanchal <svpanchal@gmail.com>
2019-03-17 09:56:48 -0400

    Adding testing frameworks; updating karma.conf to work with new testing fws

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -3,14 +3,35 @@
 
 module.exports = function (config) {
   config.set({
+    preprocessors: {
+      '**/*.tsx': ['karma-typescript']
+    },
+    typescriptPreprocessor: {
+      // options passed to the typescript compiler
+      options: {
+        sourceMap: false, // (optional) Generates corresponding .map file.
+        target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
+        module: 'amd', // (optional) Specify module code generation: 'commonjs' or 'amd'
+        noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type.
+        noResolve: true, // (optional) Skip resolution and preprocessing.
+        removeComments: true, // (optional) Do not emit comments to output.
+        concatenateOutput: false // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false.
+      },
+      // transforming the filenames
+      transformPath: function (path) {
+        return path.replace(/\.ts$/, '.js');
+      }
+    },
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular', 'sinon'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-typescript'),
+      require('karma-sinon')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
